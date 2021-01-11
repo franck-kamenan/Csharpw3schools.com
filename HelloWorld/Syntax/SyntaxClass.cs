0713a17a7506d6eb4ce9d8f10cafd816e1362fd7
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-11 12:35:49 +0100

    WriteLine et Write

---
+++ HelloWorld/Syntax/SyntaxClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Syntax
+{
+    class SyntaxClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I will print on a new line.");
+
+            Console.Write("Hello World!");
+            Console.Write("I will print on the same line.");
+        }
+    }
+}
