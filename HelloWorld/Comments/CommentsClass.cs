3ba10fcc3fa0c7a3b786631ce038f8fa4472fea4
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-11 14:29:54 +0100

    Comments

---
+++ HelloWorld/Comments/CommentsClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Comments
+{
+    class CommentsClass
+    {
+        static void Main(string[] args)
+        {
+            //This is a comment
+            Console.WriteLine("Hello World!"); //This is a comment
+            /* The code above
+            is amazing */
+        }
+    }
+}
