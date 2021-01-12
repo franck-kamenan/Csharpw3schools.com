1d76f4b8ed625a8856ca2441b7a95e0c7f55cabf
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-12 21:14:09 +0100

    If Else and Ternary Operator

---
+++ HelloWorld/IfElse/IfElseClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.IfElse
+{
+    class IfElseClass
+    {
+
+        static void Main(string[] args)
+        {
+
+            int time = 9;
+            if (time < 10)
+            {
+
+                Console.WriteLine("Good morning.");
+
+            }
+            else if (time < 20)
+            {
+
+                Console.WriteLine("Good day.");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Good evening.");
+
+            }
+
+                Console.WriteLine();
+
+            // Ternary Operator
+                Console.WriteLine((time < 18)? "Good day.": "Good evening.");
+        
+        }
+
+    }
+}
