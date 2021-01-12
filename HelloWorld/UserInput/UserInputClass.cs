b930a483e2e3dee5fc9b50141a25ceb98cad0d37
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-12 12:08:12 +0100

    User Input

---
+++ HelloWorld/UserInput/UserInputClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.UserInput
+{
+    class UserInputClass
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your username:");
+            string username = Console.ReadLine();
+            Console.WriteLine("Your username is: " + username);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+
+        }
+
+    }
+}
