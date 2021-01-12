6b8bd08ccad3ec0b85f1c1e798f2071af2c37ab8
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-12 15:22:25 +0100

    Math

---
+++ HelloWorld/MathClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class MathClass
+    {
+
+        static void Main(string[] args)
+        {
+
+            int x = 10;
+            int y = 5;
+            int nintendo = 64;
+            double negative = -4.7;
+            double virgule = 9.99;
+            Console.WriteLine(Math.Max(x, y));
+            Console.WriteLine(Math.Min(x, y));
+            Console.WriteLine(Math.Sqrt(nintendo));
+            Console.WriteLine(Math.Abs(negative));
+            Console.WriteLine(Math.Round(virgule));
+
+        }
+
+    }
+}
