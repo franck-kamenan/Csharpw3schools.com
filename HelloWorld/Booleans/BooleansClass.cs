c42f0804991b092dd1cbb5cf0e5fa6a85d215fc8
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-12 20:46:36 +0100

    Booleans

---
+++ HelloWorld/Booleans/BooleansClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Booleans
+{
+    class BooleansClass
+    {
+
+        static void Main(string[] args)
+        {
+
+            // Boolean Values
+            bool myBool = true;
+            bool isCSharpFun = true;
+            bool isFishTasty = false;
+            Console.WriteLine(myBool);
+            Console.WriteLine(isCSharpFun);
+            Console.WriteLine(isFishTasty);
+
+            Console.WriteLine();
+
+            // Boolean Expression
+            int x = 10;
+            int y = 9;
+            Console.WriteLine( x > y);
+            Console.WriteLine( 3 < 2);
+            Console.WriteLine( x == 10);
+
+        }
+
+    }
+}
