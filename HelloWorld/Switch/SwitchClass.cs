861ab8bd283ce645e9bd597858c704c445b14605
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-13 09:50:23 +0100

    Switch

---
+++ HelloWorld/Switch/SwitchClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Switch
+{
+    class SwitchClass
+    {
+
+        static void Main(string[] args)
+        {
+
+            int day = 4;
+            switch(day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Mysterious day");
+                    break;
+            }
+
+        }
+
+    }
+}
