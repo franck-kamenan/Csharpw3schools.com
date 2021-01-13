8001b1722478dcddee32f0ad095045b4a8ede0f8
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-13 10:16:12 +0100

    While Loop et Do While Loop

---
+++ HelloWorld/WhileLoop/WhileLoopClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.WhileLoop
+{
+    class WhileLoopClass
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+                Console.WriteLine();
+
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j < 5);
+        }
+    }
+}
