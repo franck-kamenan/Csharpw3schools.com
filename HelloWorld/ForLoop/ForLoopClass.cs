70cdb671c302dbbf17307c82228b495888f9202e
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-13 10:31:05 +0100

    For Loop et Foreach Loop

---
+++ HelloWorld/ForLoop/ForLoopClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.ForLoop
+{
+    class ForLoopClass
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+                Console.WriteLine();
+
+            // For Each Loop, exclusively for arrays
+            string[] cars = { "Volvo", "BMW", "Mazda" };
+            foreach(string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
