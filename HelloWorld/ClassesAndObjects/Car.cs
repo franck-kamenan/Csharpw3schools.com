152678a60f678ab3fd891716b5dd4ba0d6f6f8cf
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-14 09:11:02 +0100

    Classes And Objects

---
+++ HelloWorld/ClassesAndObjects/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.ClassesAndObjects
+{
+    class Car
+    {
+        public string color = "red";
+        public int maxSpeed = 200;
+        public string secondColor;
+        public int minSpeed;
+        public string model;
+        public int year;
+
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+    }
+}
---
+++ HelloWorld/ClassesAndObjects/ClassesAndObjectsClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.ClassesAndObjects
+{
+    class ClassesAndObjectsClass
+    {
+        static void Main(string[] args)
+        {
+            Car carObject = new Car();
+            carObject.fullThrottle();
+            carObject.secondColor = "blue";
+            carObject.minSpeed = 30;
+            Console.WriteLine(carObject.color);
+            Console.WriteLine(carObject.maxSpeed);
+            Console.WriteLine(carObject.secondColor);
+            Console.WriteLine(carObject.minSpeed);
+            Console.WriteLine();
+
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "black";
+            Ford.year = 1969;
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+        }
+    }
+}
