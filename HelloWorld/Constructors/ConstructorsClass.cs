72462bec495f91637d83f33449cf7e85baa76262
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-14 10:21:39 +0100

    Constructors

---
+++ HelloWorld/Constructors/ConstructorsClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Constructors
+{
+    class ConstructorsClass
+    {
+        static void Main(string[] args)
+        {
+            SecondCarClass ford = new SecondCarClass();
+            Console.WriteLine(ford.model);
+
+            SecondCarClass mercedes = new SecondCarClass("SLK");
+            Console.WriteLine(mercedes.model);
+
+            SecondCarClass opel = new SecondCarClass("Astra", "Red");
+            Console.WriteLine("it is a " + opel.color + " " + opel.model);
+        }
+    }
+}
---
+++ HelloWorld/Constructors/SecondCarClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Constructors
+{
+    class SecondCarClass
+    {
+        public string model;
+        public string color;
+
+        public SecondCarClass()
+        {
+            model = "Mustang";
+        }
+
+        public SecondCarClass(string modelName)
+        {
+            model = modelName;
+        }
+
+        public SecondCarClass(string model, string color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
