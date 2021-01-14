93ccb0f36762df37839ef67bc0858caecdaf1e92
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-14 11:31:41 +0100

    Properties And Encapsulation

---
+++ HelloWorld/PropertiesAndEncapsulation/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.PropertiesAndEncapsulation
+{
+    class Person
+    {
+        private string name;
+
+        // Property
+        public string Name
+        {
+            get { return name; }
+            set { name = value; } // value est le nom obligatoire
+        }
+
+        // Automatic Properties(Short Hand), no need for Fields/Attributes/Variables
+        public int Age
+        { get; set; }
+    }
+}
---
+++ HelloWorld/PropertiesAndEncapsulation/PropertiesAndEncapsulationClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.PropertiesAndEncapsulation
+{
+    class PropertiesAndEncapsulationClass
+    {
+        static void Main(string[] args)
+        {
+            Person somebody = new Person();
+            somebody.Name = "John";
+            somebody.Age = 36;
+            Console.WriteLine(somebody.Name);
+            Console.WriteLine(somebody.Age);
+        }
+    }
+}
