cf43631464a2359ba899856b79f48c42029274e1
Franck Kamenan <franck.kamenan@yahoo.com>
2021-01-16 12:18:34 +0100

    Files

---
+++ HelloWorld/Files/FilesClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;  // include the System.IO namespace
+
+namespace HelloWorld.Files
+{
+    class FilesClass
+    {
+        static void Main(string[] args)
+        {
+            string myText = "Hello World!";
+            File.WriteAllText("filename.txt", myText);
+
+            string readMyText = File.ReadAllText("filename.txt");
+            Console.WriteLine(readMyText);
+        }
+    }
+}
